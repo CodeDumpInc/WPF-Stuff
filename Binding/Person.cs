276cf0b4ea1b697f698a00851f686839caf8b80d
weeska <weeska@gmail.com>
2015-05-19 21:28:51 +0200

    Added examples for Binding and Control Templates

---
+++ Binding/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+
+        ~Person()
+        {
+            Console.WriteLine(FirstName + " " + this.LastName);
+        }
+    }
+}
